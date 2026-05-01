4cb7042446ec2a2b1962d0d0c45afba7937c4928
lemonsync <grouplemon0@gmail.com>
2026-05-02 03:39:58 +0700

    akhirnya

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
     "http://localhost:5173", 
     "http://localhost:3000", 
     "http://127.0.0.1:5173",
-    "https://lemon-expo-frontend.vercel.app"
+    "https://expo-smkn9medan.vercel.app"
 ];
 
 // Middleware CORS Utama
